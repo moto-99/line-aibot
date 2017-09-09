9d28086476d93b16cd906bb78d46ead0c94049a9
moto-99 <moto.arikitari@gmail.com>
2017-09-09 09:01:46 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+
+//POSTメソッドの取得
+//$inputString = file_get_contents('php://input');
+//error_log($inputString);
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv(
+  'CHANNEL_ACCESS_TOKEN'
+  )) ;
+$bot = new \LINE\LINEBot($httpClient,['channelSecret' => getenv(
+  'CHANNEL_SECRET'
+  )]);
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
+
+foreach ($events as $event) {
+  # code...
+  $bot -> replyText($event->getReplyToken(),'TextMessage');
+}
+
+?>
