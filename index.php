652ca477a681b57441ca034a232572c8506e9484
moto-99 <moto.arikitari@gmail.com>
2017-09-09 09:40:25 +0900

    一新

--- index.php
+++ index.php
@@ -3,26 +3,24 @@
 //Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 require_once('lineBasicFuncions.php');
-//POSTメソッドの取得
-//$inputString = file_get_contents('php://input');
-//error_log($inputString);
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv(
-  'CHANNEL_ACCESS_TOKEN'
-  )) ;
-$bot = new \LINE\LINEBot($httpClient,['channelSecret' => getenv(
-  'CHANNEL_SECRET'
-  )]);
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+// LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
 
+// 署名が正当かチェック。正当であればリクエストをパースし配列へ
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+// 配列に格納された各イベントをループで処理
 foreach ($events as $event) {
-  # code...
-  //$bot->replyText($event->getReplyToken(),'TextMessage');
-  replyTextMessage($bot,$event->getReplyToken(),'TextMessage');
-}
-
-
+  // イベントがPostbackEventクラスのインスタンスであれば
+  if ($event instanceof \LINE\LINEBot\Event\PostbackEvent) {
+    // テキストを返信し次のイベントの処理へ
+    replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
+    continue;
+  }
 
 
 ?>
