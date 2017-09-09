08d6685b5be3470f08d084ff675a95e55305e51c
moto-99 <moto.arikitari@gmail.com>
2017-09-09 18:29:04 +0900

    ミス修正

--- oumu.php
+++ oumu.php
@@ -3,12 +3,12 @@ function oumu($event,$bot){
   // MessageEventクラスのインスタンスでなければ処理をスキップ
   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
     error_log('Non message event has come');
-    continue;
+    return;
   }
   // TextMessageクラスのインスタンスでなければ処理をスキップ
   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
     error_log('Non text message has come');
-    continue;
+    return;
   }
   // オウム返し
   $bot->replyText($event->getReplyToken(), $event->getText());
