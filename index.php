70de4cbc8ce6582edb311c6f663da9506d22cf1b
moto-99 <moto.arikitari@gmail.com>
2017-09-09 10:37:15 +0900

    オウム返し

--- index.php
+++ index.php
@@ -4,19 +4,43 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/lineBasicFuncions.php';
 
+/* 最初のおまじない */
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
 // 署名が正当かチェック。正当であればリクエストをパースし配列へ
-$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+// 不正であれば例外の内容を出力
+try {
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+}
+
+
+/*  ユーザへのアクション */
 // 配列に格納された各イベントをループで処理
 foreach ($events as $event) {
-  // テキストを返信し次のイベントの処理へ
-  replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
+  // MessageEventクラスのインスタンスでなければ処理をスキップ
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+    error_log('Non message event has come');
+    continue;
+  }
+  // TextMessageクラスのインスタンスでなければ処理をスキップ
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+    error_log('Non text message has come');
+    continue;
+  }
+  // オウム返し
+  $bot->replyText($event->getReplyToken(), $event->getText());
 }
 
 
