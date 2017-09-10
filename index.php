0a0efa0b54c1c85e29c377e20885bfa4fd6308fc
moto-99 <moto.arikitari@gmail.com>
2017-09-10 22:13:23 +0900

    ミス修正中”

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ foreach ($events as $event) {
       if(strpos($event->getText(),'python') !== False){
         error_log('debag:user change python lib check');
         exec('python ' . __DIR__ . '/pythonLib_checker.py', $outpara);//python 呼び出し
-        error_log('python lib:'. $outpara);
+        error_log( $outpara);
         $bot->replyText($event->getReplyToken(), 'pythonライブラリを表示します。'. $outpara);
         continue;//ブレイクがまずいかも
       }
